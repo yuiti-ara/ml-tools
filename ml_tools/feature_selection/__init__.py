1d298c03b216a3e22d4708fd671a11f806a0bb37
yuiti-ara <yuiti.usp@gmail.com>
2018-10-12 16:12:57 -0300

    update UncorrFilter

--- ml_tools/feature_selection/__init__.py
+++ ml_tools/feature_selection/__init__.py
@@ -1,40 +1,48 @@
+from itertools import permutations
+
+import pandas as pd
 from sklearn.pipeline import TransformerMixin
-from sklearn.ensemble import RandomForestRegressor
-from rfpimp import importances, oob_dependences
+from rfpimp import importances
 
 
 class FilterCorr(TransformerMixin):
     def __init__(self):
-        self.cols = []
+        self.cols_selected = []
 
     def fit(self, df):
-        df_corr = df.corr(method='spearman')
+        df_corr = df.corr(method='spearman').abs()
 
-        cols_bad = set()
+        groups = []
         for idx, (col, row) in enumerate(df_corr.iterrows()):
-            series = row[idx + 1:].abs() > .5
-            if series.sum() > 0:
-                cols_bad.add(col)
-        self.cols = list(set(df.columns) - cols_bad)
+            row = row[idx + 1:]
+            row = row[row > .5]
+            if not row.empty:
+                group = {col, *row.index}
+                groups.append(group)
 
+        to_drop = set()
+        for group in groups:
+            df_combs = pd.DataFrame(list(permutations(group, 2)))
+
+            def fn(x):
+                corr = df_corr.loc[x[0], x[1]]
+                if corr > .5:
+                    return corr
+
+            df_combs['corr'] = df_combs.apply(fn, axis='columns')
+
+            df_counts = df_combs.groupby(0)['corr'].agg(['count', 'sum'])
+            df_counts = df_counts.sort_values(['count', 'sum'], ascending=False)
+
+            row = df_counts.iloc[0, :]
+            if row['count'] == len(df_counts) - 1:
+                to_drop |= set(df_counts.iloc[1:, 0].index)
+
+        self.cols_selected = list(set(df.columns) - to_drop)
         return self
 
     def transform(self, df):
-        return df[self.cols]
-
-
-class FilterDependent(TransformerMixin):
-    def __init__(self):
-        self.cols = []
-
-    def fit(self, df):
-        model = RandomForestRegressor(n_estimators=50, n_jobs=-1, oob_score=True)
-        deps = oob_dependences(model, df)
-        self.cols = list(deps[deps.Dependence < 0].index)
-        return self
-
-    def transform(self, df):
-        return df[self.cols]
+        return df[self.cols_selected]
 
 
 def rfe(model, df_tr, y_tr, df_vl, y_vl):
