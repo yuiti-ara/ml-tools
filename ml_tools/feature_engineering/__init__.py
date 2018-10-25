8243e5c9d7891185fc48a27f1996e5bf43da268e
yuiti-ara <yuiti.usp@gmail.com>
2018-10-24 22:20:44 -0300

    remove unused import

--- ml_tools/feature_engineering/__init__.py
+++ ml_tools/feature_engineering/__init__.py
@@ -2,6 +2,5 @@ from .pandas_feature_union import PandasFeatureUnion
 from .categoricals import pipe_cat
 from .numericals import pipe_num
 from .dates import pipe_datecycles, pipe_dateparts, pipe_datena
-from .dummies import pipe_dummy
 from .raws import pipe_raw
 
