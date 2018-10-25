69176effafeed7f079365e03283211e60e24719a
yuiti-ara <yuiti.usp@gmail.com>
2018-10-24 21:53:53 -0300

    update categoricals

--- ml_tools/feature_engineering/categoricals.py
+++ ml_tools/feature_engineering/categoricals.py
@@ -1,29 +1,36 @@
-# import pandas as pd
-#
-# from sklearn.pipeline import TransformerMixin
-# from sklearn_pandas import DataFrameMapper
-# from sklearn_pandas.categorical_imputer import CategoricalImputer
-#
-#
-# class FreqEncoder(TransformerMixin):
-#     def __init__(self):
-#         self.mapper = {}
-#
-#     def fit(self, x):
-#         s = pd.Series(x)
-#         self.mapper = s.value_counts(dropna=False, normalize=True).to_dict()
-#         return self
-#
-#     def transform(self, x):
-#         s = pd.Series(x)
-#         return s.apply(lambda x: self.mapper.get(x, 0))
-#
-#
-# def pipe_cat(cols):
-#     pipe = DataFrameMapper([
-#         (col, [
-#             CategoricalImputer(strategy='fixed_value', replacement='_'),
-#             FreqEncoder()
-#         ]) for col in cols
-#     ], df_out=True)
-#     return pipe
+import pandas as pd
+
+from sklearn.pipeline import TransformerMixin
+from sklearn.preprocessing import FunctionTransformer
+from sklearn_pandas import DataFrameMapper
+
+
+class FreqEncoder(TransformerMixin):
+    def __init__(self):
+        self.mapper = {}
+
+    def fit(self, x):
+        s = pd.Series(x)
+        self.mapper = s.value_counts(normalize=True).to_dict()
+        return self
+
+    def transform(self, x):
+        s = pd.Series(x)
+        return s.apply(lambda x: self.mapper.get(x, 0))
+
+
+def to_str(x):
+    idx = pd.isnull(x)
+    x = x.astype(str)
+    x[idx] = '_'
+    return x
+
+
+def pipe_cat(cols):
+    pipe = DataFrameMapper([
+        (col, [
+            FunctionTransformer(to_str, validate=False),
+            FreqEncoder(),
+        ]) for col in cols
+    ], df_out=True)
+    return pipe
